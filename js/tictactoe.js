bb464d89d289028ab10e6dd66e5e7fb06edcc5ee
Kenya Chan <kenya.chan@gmail.com>
2022-10-23 23:57:04 +1100

    Add check for winner function

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,4 +1,6 @@
 const Player = (name, sign) => {
+    const moves = [];
+    
     const _name = name;
     const _sign = sign;
     
@@ -10,22 +12,25 @@ const Player = (name, sign) => {
         return _sign;
     }
 
+    const addMove = (position) => {
+        moves.push(position);
+    }
+
+    const getMoves = () => {
+        return moves;
+    }
+
     return {
-        getName, getSign,
+        getName, getSign, addMove, getMoves,
     }
 
 };
 
-const Game = (() => {
+const Board = (squares) => {
     const board = ['','','','','','','','',''];
-    const player1 = Player('Player 1', 'X');
-    const player2 = Player('Player 2', 'O');
-    let activePlayer = player1;
+    const _squares = squares;
 
-    const squares = document.querySelectorAll('#game .square');  
-    const resetButton = document.querySelector('#game #reset-button');
-
-    const _add = (sign, position) => {
+    const add = (sign, position) => {
         if (position < 0 || position > 8) return;
         if (board[position] !== '') return;
         
@@ -34,41 +39,76 @@ const Game = (() => {
     }
 
     const _render = () => {
-        squares.forEach((square) => {
+        _squares.forEach((square) => {
             square.textContent = board[square.dataset.index];
         });
     }
 
     const reset = () => {
-        squares.forEach((square) => {
+        _squares.forEach((square) => {
             square.disabled = false;
             board[square.dataset.index] = '';
         });
 
-        activePlayer = player1;
+        // activePlayer = player1;
 
         _render();
     }
+
+    const getBoard = () => {
+        return board;
+    }
+
+    return {
+        add, reset, getBoard,
+    };
+};
+
+
+const Game = (() => {
+    const squares = document.querySelectorAll('#game .square');  
+    const resetButton = document.querySelector('#game #reset-button');
+    
+    const board = Board(squares);
+    const winningCombos = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']];
+    
+    const player1 = Player('Player 1', 'X');
+    const player2 = Player('Player 2', 'O');
+    let activePlayer = player1;
+
+
+    // resetButton.addEventListener('click', () => board.reset());
+    
 
     const _toggleActivePlayer = () => {
         activePlayer = activePlayer === player1 ? player2 : player1;
     }
 
-    // consider storing positions in the player object and doing a check using the array method contains all? 
     const checkWinner = () => {
-        if ((board[0] !== '') && (board[0] === board[1] && board[0] === board[2])) {
-            console.log("Chicken dinner!");
-
-            return activePlayer;
-        }
+        let activePlayerMoves = activePlayer.getMoves();
+        let win = null;
         
+        if (activePlayerMoves.length < 3) 
+            return null;
+
+        win = !winningCombos.every(combo => {
+            let containsWinningCombo = !combo.every(value => activePlayerMoves.includes(value));
+
+            if (containsWinningCombo === true)
+                return true;
+        });
+
+        if (win === true) 
+            return activePlayer;
+
         return null;
     }
 
     // game logic/flow
     squares.forEach((square) => {
         square.addEventListener('click', (event) => {
-            _add(activePlayer.getSign(), event.target.dataset.index);
+            activePlayer.addMove(event.target.dataset.index);
+            board.add(activePlayer.getSign(), event.target.dataset.index);
             event.target.disabled = true;
             
             let winner = checkWinner();
@@ -79,14 +119,8 @@ const Game = (() => {
                 _toggleActivePlayer();
             }
 
-            _render();
         })
     });
 
-    resetButton.addEventListener('click', reset);
-
-    return {
-        reset, board,
-    }
 })();
 
