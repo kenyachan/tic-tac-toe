471582f2748f9be3a26371841bcae165c5ca10d1
Kenya Chan <kenya.chan@gmail.com>
2022-10-27 10:05:57 +1100

    Refactor

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,121 +1,161 @@
+const Board = ((squares) => {
+    const _board = ['','','','','','','','',''];
+    const _squares = squares;
+
+    const play = (sign, position) => {
+        if (position < 0 || position > 8) return;
+        
+        _board[position] = sign;
+        disable(position);
+        _render();
+    }
+
+    const _render = () => {
+        _squares.forEach((square) => {
+            square.textContent = _board[square.dataset.index];
+        });
+    }
+
+    const reset = () => {
+        _squares.forEach((square) => {
+            square.disabled = false;
+            _board[square.dataset.index] = '';
+        });
+
+        _render();
+    }
+
+    const getBoard = () => {
+        return _board;
+    }
+
+    const enable = (position) => {
+        if (position === undefined) {
+            _squares.forEach(square => {
+                square.disabled = false;
+            });
+
+            return true;
+        }
+
+        _squares.forEach(square => {
+            if (square.dataset.index === position) {
+                square.disabled = false;
+            }
+        });
+
+        return position;
+    }
+
+    const disable = (position) => {
+        if (position === undefined) {
+            _squares.forEach(square => {
+                square.disabled = true;
+            });
+
+            return true;
+        }
+
+        _squares.forEach(square => {
+            if (square.dataset.index === position) {
+                square.disabled = true;
+            }
+        });
+
+        return position;
+    }
+
+    const isFull = () => {
+        let full = _board.every(square => {
+            return square !== '';
+        })
+
+        return full === true;
+    }
+
+    return {
+        play, reset, getBoard, disable, enable, isFull,
+    };
+});
+
+const Player = (name, sign) => {
+    const _moves = [];
+    const _name = name;
+    const _sign = sign;
+    
+    const getName = () => {
+        return _name;
+    }
+
+    const getSign = () => {
+        return _sign;
+    }
+
+    const addMove = (position) => {
+        _moves.push(position);
+    }
+
+    const getMoves = () => {
+        return _moves;
+    }
+
+    return {
+        getName, getSign, addMove, getMoves,
+    }
+
+};
+
+const Modal = (() => {
+    const overlay = document.querySelector('.modal-widget .overlay');
+    const modalForm = document.querySelector('.modal-widget .modal')
+    const newGameButton = document.querySelector('.modal-widget #new-game-form #new-game-button');
+
+    newGameButton.addEventListener('click', ()=> {
+        _close();
+        newGame();
+    });
+
+    const _close = () => {
+        overlay.classList.toggle('active');
+        modalForm.classList.toggle('active');
+    }
+
+    const newGame = () => {
+        TicTacToe.newGame(Player(playerOneName.value, 'X'), Player(playerTwoName.value, 'O'));
+    }
+
+    return {
+        newGame,
+    }
+})();
+
 const TicTacToe = (() => {
     const squares = document.querySelectorAll('#game .square');
     const resetButton = document.querySelector('#game #reset-button');
     const players = document.querySelectorAll('#game .player');
     const outcomeText = document.querySelector('#outcome-text');
     
-    let board;
     const winningCombos = [['0', '1', '2'], ['3', '4', '5'], ['6', '7', '8'], ['0', '3', '6'], ['1', '4', '7'], ['2', '5', '8'], ['0', '4', '8'], ['2', '4', '6']];
     
-    let player1;
-    let player2;
-    let activePlayer;
+    let _player1;
+    let _player2;
+    let _activePlayer;
+    let board;
     
-    const Player = (name, sign) => {
-        const moves = [];
-        
-        const _name = name;
-        const _sign = sign;
-        
-        const getName = () => {
-            return _name;
-        }
-    
-        const getSign = () => {
-            return _sign;
-        }
-    
-        const addMove = (position) => {
-            moves.push(position);
-        }
-    
-        const getMoves = () => {
-            return moves;
-        }
-    
-        return {
-            getName, getSign, addMove, getMoves,
-        }
-    
-    };
 
-    const Board = () => {
-        const board = ['','','','','','','','',''];
-        const squares = document.querySelectorAll('#game .square'); 
-    
-        const add = (sign, position) => {
-            if (position < 0 || position > 8) return;
-            if (board[position] !== '') return;
-            
-            board[position] = sign;
-            _render();
-        }
-    
-        const _render = () => {
-            squares.forEach((square) => {
-                square.textContent = board[square.dataset.index];
-            });
-        }
-    
-        const reset = () => {
-            squares.forEach((square) => {
-                square.disabled = false;
-                board[square.dataset.index] = '';
-            });
-    
-            _render();
-        }
-    
-        const getBoard = () => {
-            return board;
-        }
-    
-        return {
-            add, reset, getBoard,
-        };
-    };
+    //
+    const newGame = (player1, player2) => {
+        _player1 = player1;
+        _player2 = player2;
+        _activePlayer = _player1;
+        board = Board(squares);
+        players[0].textContent = `${_player1.getName()}: ${_player1.getSign()}`;
+        players[1].textContent = `${_player2.getName()}: ${_player2.getSign()}`;
+    }
 
-    const Modal = (() => {
-        const overlay = document.querySelector('.modal-widget .overlay');
-        const modalForm = document.querySelector('.modal-widget .modal')
-        const newGameButton = document.querySelector('.modal-widget #new-game-form #new-game-button');
-    
-        newGameButton.addEventListener('click', ()=> {
-            close();
-            newGame();
-        });
-    
-        const close = () => {
-            overlay.classList.toggle('active');
-            modalForm.classList.toggle('active');
-        }
-    
-    
-        const newGame = () => {
-            // set player names
-            player1 = Player(playerOneName.value, 'X');
-            player2 = Player(playerTwoName.value, 'O');
-            activePlayer = player1;
-
-            players[0].textContent = `${player1.getName()}: ${player1.getSign()}`
-            players[1].textContent = `${player2.getName()}: ${player2.getSign()}`
-
-            board = Board();
-        }
-
-        return {
-            newGame,
-        }
-    })();
-
-    const resetGame = () => {
-        board.reset();
-        
-        // player1 = Player('Player 1', 'X');
-        // player2 = Player('Player 2', 'O');
-
-        // activePlayer = player1;
-
+    //
+    const reset = () => {
+        Board.reset();
         Modal.newGame();
 
         players[0].classList.add('active');
@@ -126,63 +166,67 @@ const TicTacToe = (() => {
     }
 
     const toggleActivePlayer = () => {
-        activePlayer = activePlayer === player1 ? player2 : player1;
+        _activePlayer = _activePlayer === _player1 ? _player2 : _player1;
 
         players.forEach((player) => {
             player.classList.toggle('active');
         });
     }
 
-    const checkWin = () => {
-        let activePlayerMoves = activePlayer.getMoves();
-        let gameBoard = board.getBoard();
+    const checkWin = (player) => {
+        let moves = player.getMoves();
 
-        let tie = gameBoard.every(move => {
-            return move ===
-                player1.getSign() || move === player2.getSign();
+        let winResult = !winningCombos.every(combo => {
+            let containsWinningCombo = !combo.every(value => moves.includes(value));
+
+            if (containsWinningCombo === true) return true;
         });
 
-        if (tie === true) return null;
-
-        let winner = !winningCombos.every(combo => {
-            let containsWinningCombo = !combo.every(value => activePlayerMoves.includes(value));
-
-            if (containsWinningCombo === true)
-                return true;
-        });
-
-        return winner;
+        return winResult;
     }
 
-    const declareWinner = (outcome) => {
-        squares.forEach((square) => {
-            square.disabled = true;
-        });
+    const declareOutcome = (player, outcome) => {
+        switch(outcome) {
+            case 'win':
+                outcomeText.textContent = `${player.getName()} wins!`;
+                break;
+            case 'tie':
+                outcomeText.textContent = 'Tie game!';
+                break;
+        }
 
-        outcomeText.textContent = outcome;
         outcomeText.classList.remove('hidden');
     }
 
-    // game logic/flow
-    squares.forEach((square) => {
-        square.addEventListener('click', (event) => {
-            activePlayer.addMove(event.target.dataset.index);
-            board.add(activePlayer.getSign(), event.target.dataset.index);
-            event.target.disabled = true;
-            
-            let win = activePlayer.getMoves().length >= 3? checkWin() : false;
+    const playRound = (player, position, board) => {
+        player.addMove(position);
+        board.play(player.getSign(), position);
+
+        if (player.getMoves().length >= 3) {
+            let win = checkWin(player);
 
             if (win === true) {
-                declareWinner(`${activePlayer.getName()} wins!`);
-            } else if (win === null) {
-                declareWinner('Tie Game');
-            } else {
-                toggleActivePlayer();
+                board.disable();
+                declareOutcome(player, 'win');
+                return;
             }
 
-        })
+            if (win === false && board.isFull() === true) {
+                declareOutcome(player, 'tie');
+                return;
+            }
+        }
+
+        toggleActivePlayer();
+    }
+
+    squares.forEach(square => {
+        square.addEventListener('click', (event) => playRound(_activePlayer, event.target.dataset.index, board));
     });
 
-    resetButton.addEventListener('click', () => resetGame());
+    resetButton.addEventListener('click', () => reset());
 
+    return {
+        newGame,
+    }
 })();
